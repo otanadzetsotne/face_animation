1ceca48d8f157bff546902ab9f32478aa1d25d5b
otanadzetsotne <otanadzetsotne@yahoo.com>
2024-04-03 13:36:09 +0400

    AdaSR-TalkingHead testing

---
+++ changed_files/AdaSR-TalkingHead/run_demo.sh
@@ -0,0 +1,7 @@
+python demo.py \
+    --config config/mix-resolution.yml \
+    --checkpoint checkpoints/mix-train.pth.tar \
+    --source_image DEMO/photo.jpeg \
+    --driving_video DEMO/YandexSample.mp4  \
+    --relative \
+    --cpu
